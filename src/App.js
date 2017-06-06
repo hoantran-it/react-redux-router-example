90cd3369d1f6c1c6cced38dcd0d68565cf911252
Hoan Tran <hoantran.it@gmail.com>
2017-06-06 14:28:02 +0700

    Apply redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Provider} from 'react-redux';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <Router routes={routes} history={browserHistory}/>
+      </Provider>
     );
   }
 }
