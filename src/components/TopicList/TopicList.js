109ff8683ed32822dbf8a0e31ce1ba8fe6bfafbb
Hoan Tran <hoantran.it@gmail.com>
2017-06-12 17:12:42 +0700

    Modify topic page

--- src/components/TopicList/TopicList.js
+++ src/components/TopicList/TopicList.js
@@ -1,7 +1,19 @@
 import React from "react";
+import {connect} from "react-redux";
+
 import TopicItem from "components/TopicItem";
+import * as topicActions from "actions/topicActions";
+
 
 class TopicList extends React.Component {
+  static defaultProps = {
+    topicList: []
+  }
+
+  componentDidMount() {
+    this.props.getTopics("aaa");
+  }
+
   render() {
     return (
       <div>
@@ -13,4 +25,16 @@ class TopicList extends React.Component {
   }
 }
 
-export default TopicList;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    topicList: state.topic.topicList
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getTopics: filter => dispatch(topicActions.getTopics(filter))
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TopicList);
--- src/pages/TopicPage/TopicPage.js
+++ src/pages/TopicPage/TopicPage.js
@@ -3,47 +3,18 @@ import {connect} from "react-redux";
 
 import CircularProgress from "material-ui/CircularProgress";
 import TopicList from "components/TopicList";
-import RaisedButton from "material-ui/RaisedButton";
-
-import * as topicActions from "actions/topicActions";
 
 
 class TopicPage extends React.Component {
 
-  static defaultProps = {
-    topicList: []
-  }
-
-  // eslint-disable-next-line
-  constructor(props){
-    super(props);
-  }
-
-  loadTopic(filter) {
-    this.props.getTopics(filter);
-  }
-
   render() {
     return (
       <div>
-        <RaisedButton onTouchTap={e => this.loadTopic("aaa")} label="Sign Up"/>
-        <TopicList topicList={this.props.topicList}/>
+        <TopicList/>
         <CircularProgress />
       </div>
     );
   }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    topicList: state.topic.topicList
-  }
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getTopics: filter => dispatch(topicActions.getTopics(filter))
-  }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(TopicPage);
+export default TopicPage;
