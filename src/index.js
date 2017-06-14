39d4aa34f7766ecdb66a730a93ad530cdf83a62f
Hoan Tran <hoantran.it@gmail.com>
2017-06-14 21:48:40 +0700

    Add foundation library

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import injectTapEventPlugin from "react-tap-event-plugin";
 import registerServiceWorker from "registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap-theme.css";
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
 import "antd/dist/antd.css";
 import "style.css";
 import App from "App";
